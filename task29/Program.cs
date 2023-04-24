d3fb539c8c839c9b739ae44c313de23b917b6232
Evgenii <joon25@mail.ru>
2023-04-24 21:01:18 +0300

    решение задачи 29

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(-10,10);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
